08b0a3f4aa2173458eb857ef4772960b43600dda
Alan Oliveira <alan.oliveira@g4flex.com.br>
2021-04-24 14:51:44 -0300

    ecosystem pronto para rodar em ambiente dev com PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,11 @@ module.exports = {
     apps: [
         {
             name: "Compasso UOL",
-            script: "./init.js",
+            script: "src/init.js",
             watch: true,
             env: {
-                "PORT": 3000,
                 "NODE_ENV": "development"
             },
-            env_production: {
-                "PORT": 4001,
-                "NODE_ENV": "production",
-            }
         }
     ]
 }
